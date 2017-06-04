19a6106c40a6b1860bb6106358a7940f9272ec60
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2017-06-04 02:14:32 +0200

    updated radar and lidar mesuraments

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -105,8 +105,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+
   } else {
     // Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    VectorXd z(2);
+    z << measurement_pack.raw_measurements_[0],
+      measurement_pack.raw_measurements_[1];
+    ekf_.Update(z);
   }
 
   // print the output
